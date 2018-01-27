aa958a591431aa4e937f90db4a92597468732af8
Faxxiz <charles.castelain@epitech.eu>
2018-01-27 15:08:00 +0100

    sku
    skusku

---
+++ resource/Post/Assets/ChangeVolume.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeVolume : MonoBehaviour {
+    public Slider volumeSlider;
+    public AudioSource volumeAudio;
+
+    public void VolumeController() {
+        if (volumeSlider.value == 0)
+            volumeAudio.volume = 0;
+        else
+            volumeAudio.volume = volumeSlider.value + (float)(0.2);
+    }
+}
